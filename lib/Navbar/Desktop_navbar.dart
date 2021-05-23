4b5cede24faa6ee0b889e2a0d14df3ae04d26c62
Leman316 <56719469+Leman316@users.noreply.github.com>
2021-05-23 17:05:13 +0600

    1.2

--- lib/Navbar/Desktop_navbar.dart
+++ lib/Navbar/Desktop_navbar.dart
@@ -3,43 +3,58 @@ import 'package:flutter/material.dart';
 class DesktopNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // constraints: BoxConstraints(maxWidth: 1200),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Text(
-            'Retro Studio',
-            style: TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'Home',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                'About us',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                'Portfolio',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-            ],
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        // constraints: BoxConstraints(maxWidth: 1200),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              'Retro Studio',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Home',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'About us',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'Portfolio',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/Navbar/Mobile_navbar.dart
+++ lib/Navbar/Mobile_navbar.dart
@@ -3,6 +3,49 @@ import 'package:flutter/material.dart';
 class MobileNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              'Retro Studio',
+              style: TextStyle(
+                  fontFamily: 'Anton',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'About us',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'Portfolio',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
